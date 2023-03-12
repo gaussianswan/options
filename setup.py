6bd8175ed56e3b3acd939309cc9f5855286b0ac4
gaussianswan <srerrie@gmail.com>
2023-03-11 20:43:06 -0500

    complete initial setup.py file

--- setup.py
+++ setup.py
@@ -1,2 +1,9 @@
 from setuptools import setup
 
+setup(name = 'options',
+      version = '0.1.0',
+      description = 'Package for options analysis',
+      author='Stephon Henry-Rerrie',
+      author_email='srerrie@gmail.com',
+      packages = ["options"]
+)
