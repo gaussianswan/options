85438623695c45f9a2e8266851f56268e23ce8bc
gaussianswan <srerrie@gmail.com>
2023-03-11 20:35:17 -0500

    fixed bug in time to maturity function

--- options/funcs.py
+++ options/funcs.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas_market_calendars as mcal
 
 from options.constants import NUMBER_OF_TRADING_DAYS
-from options.positions import Position
 
 from datetime import date
 from typing import List
@@ -12,7 +11,7 @@ nyse_calendar = mcal.get_calendar("NYSE")
 def time_to_expiry(expiry_date: date, start_date: date, calendar = nyse_calendar) -> float:
 
     market_calendar = calendar.schedule(start_date = start_date, end_date = expiry_date)
-    number_of_days = market_calendar
+    number_of_days = market_calendar.shape[0]
 
     tte = number_of_days / NUMBER_OF_TRADING_DAYS
 
